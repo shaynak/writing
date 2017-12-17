3d761c4389b874b9bedfe208c69d6701678b1e1d
shaynak <shayna.kothari@gmail.com>
2017-12-16 23:50:49 -0800

    added some pieces and functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+//var hid = true;
+/*$(document).ready(function() {
+	"use strict";
+	if (hid) {
+		hideBody();
+	}
+});
+
+function hideBody() {
+	"use strict";
+	$(".body").hide();
+	hid = false;
+}
+
+function visibility(section) {
+	"use strict";
+	$(".body").not("#" + section).hide();
+    $("#" + section).show();
+}*/
